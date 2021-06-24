16fc7c44e6a04db9750e24cf7ad1b9c848c36074
Omar <omar0940@gmail.com>
2021-06-24 14:26:21 +0200

    add API ID & Key & link

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,6 +1,10 @@
 import React from "react";
 
 const Recipe = () => {
+  let API_link =
+    "https://developer.edamam.com/edamam-docs-recipe-api#!/Recipe_Search/get_api_recipes_v2";
+  let App_ID = "1b14ca3f";
+  let API_KEY = "d8fd4b9340a8fb07b62b35013de596c4";
   return <div></div>;
 };
 
