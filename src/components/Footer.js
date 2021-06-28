a0f033a317fd3b65afe26c3423b73bea4afa2668
Omar <omar0940@gmail.com>
2021-06-28 09:49:49 +0200

    working on Footer JS

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Footer = () => {
-  return <div></div>;
+  let year = new Date().getFullYear();
+  return (
+    <div className="footer__container">
+      <h4>Copyrights ®{year} omNomNom Recipe's Website. All Rights Reserved</h4>
+    </div>
+  );
 };
 
 export default Footer;
