9bd2e5eacadcb89ae4ebf3d2a447c0513b8a91c2
Omar <omar0940@gmail.com>
2021-06-28 14:02:35 +0200

    Working on NavBar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,28 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Nav = () => {
-  return <div></div>;
+  return (
+    <div className="navBar__container">
+      <div className="navLogo"></div>
+      <div className="navBar">
+        <ul>
+          <Link to="/">
+            <li>HOMEPAGE</li>
+          </Link>
+          <Link to="/Recipe">
+            <li>RECIPES</li>
+          </Link>
+          <Link to="/About">
+            <li>ABOUT US</li>
+          </Link>
+          <Link to="/Contact">
+            <li>CONTACT US</li>
+          </Link>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Nav;
