e6f886c66b6be9a93298c27bef1d25172dbdb436
MaryamPayenda <maryampayanda12@gmail.com>
2021-06-28 11:49:59 +0200

    API

--- src/App.js
+++ src/App.js
@@ -1,5 +1,68 @@
-function App() {
-  return <div className="App"></div>;
-}
+import React, { useState, useEffect } from "react";
+import Recipe from "./components/Recipe";
+//import Search from "./components/Search";
+// import SearchResult from "./components/SearchResult";
+
+const App = () => {
+  const APP_ID = "271b281a";
+  const APP_KEY = "88c627abf78667444cf4d804190f1b2c";
+
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("Arabic");
+
+  useEffect(async () => {
+    const getRecipes = async () => {
+      const response = await fetch(
+        `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
+      );
+      const data = await response.json();
+      console.log(data.hits);
+      setRecipes(data.hits);
+    };
+
+    getRecipes();
+  }, [query]);
+
+  const handleSearch = (e) => {
+    setSearch(e.target.value);
+    // console.log(search)
+  };
+  const getSearch = (e) => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch("");
+  };
+
+  return (
+    <div>
+      {/* <Search /> */}
+      <form className="search-form" onSubmit={getSearch}>
+        <input
+          type="text"
+          className="search-bar"
+          value={search}
+          onChange={handleSearch}
+          placeholder="Search..."
+        />
+        <button type="submit" className="search-btn">
+          Search
+        </button>
+      </form>
+      <div className="container">
+        {recipes.map((recipe, index) => (
+          <Recipe
+            key={index}
+            title={recipe.recipe.label}
+            cuisine={recipe.recipe.cuisineType}
+            calories={recipe.recipe.calories}
+            image={recipe.recipe.image}
+            ingredients={recipe.recipe.ingredients}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default App;
--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,7 +1,20 @@
 import React from "react";
 
-const Recipe = () => {
-  return <div></div>;
+const Recipe = ({ title, calories, cuisine, image, ingredients }) => {
+  return (
+    <div>
+      <h1>{title}</h1>
+      <img src={image} alt="recipe-image" />
+      <h3>Cuisine Type {cuisine}</h3>
+      <h3>Calories : {calories}</h3>
+      <h3>Ingredients:</h3>
+      <ol>
+        {ingredients.map((ingredient, i) => (
+          <li key={i}>{ingredient.text}</li>
+        ))}
+      </ol>
+    </div>
+  );
 };
 
 export default Recipe;
