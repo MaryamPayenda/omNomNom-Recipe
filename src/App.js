8376d660b547f54fe8c3e57fc809341a69ef3f9c
catha <catharinaczipf@hotmail.com>
2021-06-28 10:54:13 +0200

    some sass stuff..

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>OmNomNom – Food & Recipes </h1>
+      <h2>OmNomNom – Food & Recipes </h2>
+      <h3>OmNomNom – Food & Recipes </h3>
+      <h4>OmNomNom – Food & Recipes </h4>
+      <h5>OmNomNom – Food & Recipes </h5>
+      <h6>OmNomNom – Food & Recipes </h6>
+      <p>all about us</p>
+    </div>
+  );
 }
 
 export default App;
