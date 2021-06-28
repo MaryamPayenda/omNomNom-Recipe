0faec5495dc6bde824aa1cd8f8fb2b197d383368
Omar <omar0940@gmail.com>
2021-06-28 09:46:16 +0200

    working on App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import React, { useState } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Nav from "./components/Nav";
+import Home from "./components/Home";
+import Footer from "./components/Footer";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <Nav />
+      <Footer />
+      <Switch></Switch>
+    </Router>
+    // <div className="App"></div>;
+  );
 }
 
 export default App;
