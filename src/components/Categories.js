fcc1441f37e2c05254744320b6556bf606e7ba2e
Omar <omar0940@gmail.com>
2021-06-29 14:58:03 +0200

    working on categories

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,12 +1,17 @@
+import axios from "axios";
 import React from "react";
 import { Link } from "react-router-dom";
 
 function Categories() {
   const APP_ID = "271b281a";
   const APP_KEY = "88c627abf78667444cf4d804190f1b2c";
-  const meat = fetch(
-    `https://api.edamam.com/search?q=meat&app_id=${APP_ID}&app_key=${APP_KEY}`
-  );
+  const url_meat = `
+    https://api.edamam.com/search?q=meat&app_id=${APP_ID}&app_key=${APP_KEY}`;
+  const vegetarian = `
+    https://api.edamam.com/search?q=vegetarian&app_id=${APP_ID}&app_key=${APP_KEY}`;
+  const url_vegan = `
+    https://api.edamam.com/search?q=vegan&app_id=${APP_ID}&app_key=${APP_KEY}`;
+  const meat = await axios.get(url_meat);
   return (
     <div className="categories">
       <Link to={meat}>
