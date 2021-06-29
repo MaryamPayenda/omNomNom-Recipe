5746d81d8c35e5a0b8a294e65d94bc66527d6139
Omar <omar0940@gmail.com>
2021-06-29 14:21:22 +0200

    working on Categories

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,11 +1,16 @@
 import React from "react";
 
 function Categories() {
+  const APP_ID = "271b281a";
+  const APP_KEY = "88c627abf78667444cf4d804190f1b2c";
+  const meat = fetch(
+    `https://api.edamam.com/search?q=meat&app_id=${APP_ID}&app_key=${APP_KEY}`
+  );
   return (
     <div className="Categories">
-      <div>Meat</div>
-      <div>Vegetarian</div>
-      <div>Vegen</div>
+      <div className="meat">Meat</div>
+      <div className="vegetarian">Vegetarian</div>
+      <div className="vegan">Vegen</div>
     </div>
   );
 }
