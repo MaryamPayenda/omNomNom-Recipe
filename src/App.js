3e1cc85fc90e09f1a4221d4398aab65f53c3e03b
MaryamPayenda <maryampayanda12@gmail.com>
2021-06-30 09:41:47 +0200

    added

--- src/App.js
+++ src/App.js
@@ -3,28 +3,19 @@ import Nav from "./components/Nav";
 import Home from "./components/Home";
 import About from "./components/About";
 import Footer from "./components/Footer";
-
 import Recipe from "./components/Recipe";
-// import SearchResult from "./components/Recipe";
-
-import React, { useState, useEffect, useRef } from "react";
-import SearchResult from "./components/SearchResult";
-
-import { RiArrowDropRightLine, RiArrowDropLeftLine } from "react-icons/ri";
 
 const App = () => {
   return (
     <div>
       <Router>
         <Nav />
-
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/Recipe" exact component={Recipe} />
           <Route path="/About" exact component={About} />
         </Switch>
       </Router>
-
       <Footer />
     </div>
   );
