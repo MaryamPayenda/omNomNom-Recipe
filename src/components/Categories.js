30fb2f4192a710513399b1fc34a3839618c72ff3
Omar <omar0940@gmail.com>
2021-06-30 23:16:20 +0200

    responcive categoreis design

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import meat_img from "../img/catogories11.jpg";
-import vegetarian_img from "../img/catogories2.jpg";
-import vegan_img from "../img/catogories3.jpg";
+import vegetarian_img from "../img/catogories22.jpg";
+import vegan_img from "../img/catogories33.jpg";
 
 function Categories() {
   return (
@@ -15,13 +15,13 @@ function Categories() {
       </Link>
       <Link to="/Dishes/vegetarian">
         <div className="vegetarian">
-          <img src={vegetarian_img} />
+          <img src={vegetarian_img} id="vegetairian_img" />
           Vegetarian
         </div>
       </Link>
       <Link to="/Dishes/vegan">
         <div className="vegan">
-          <img src={vegan_img} />
+          <img src={vegan_img} id="vegan_img" />
           Vegen
         </div>
       </Link>
