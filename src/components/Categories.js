2bdb2472398d2d0cc6911f68594748d67b5012d8
Omar <omar0940@gmail.com>
2021-06-30 09:31:19 +0200

    uninstall axois

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,28 +1,27 @@
+import React, { useState, useEffect } from "react";
 import Axios from "axios";
-import React from "react";
 import { Link } from "react-router-dom";
 
 function Categories() {
   const APP_ID = "271b281a";
   const APP_KEY = "88c627abf78667444cf4d804190f1b2c";
-  const url_meat = `
-    https://api.edamam.com/search?q=meat&app_id=${APP_ID}&app_key=${APP_KEY}`;
-  const vegetarian = `
-    https://api.edamam.com/search?q=vegetarian&app_id=${APP_ID}&app_key=${APP_KEY}`;
-  const url_vegan = `
-    https://api.edamam.com/search?q=vegan&app_id=${APP_ID}&app_key=${APP_KEY}`;
-  const meat = async () => await Axios.get(url_meat);
+  const url = `
+    https://api.edamam.com/search?q=${type}&app_id=${APP_ID}&app_key=${APP_KEY}`;
+  const meat = async () => await Axios.get(url);
   console.log(meat);
   return (
     <div className="categories">
       <Link to={meat}>
+        <Dishes type="meat" />
         <div className="meat">Meat</div>
       </Link>
       <Link>
         <div className="vegetarian">Vegetarian</div>
+        {/* <Dishes type="Vegetarian" /> */}
       </Link>
       <Link>
         <div className="vegan">Vegen</div>
+        {/* <Dishes type="vegan"></Dishes> */}
       </Link>
     </div>
   );
---
+++ src/components/Dishes.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Dishes() {
+  return <div></div>;
+}
+
+export default Dishes;
