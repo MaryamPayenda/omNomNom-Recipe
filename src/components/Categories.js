605fd2b2f2cddabeb3cb1cc27390861df499550a
Omar <omar0940@gmail.com>
2021-06-30 16:20:21 +0200

    categories design

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,17 +1,29 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import meat_img from "../img/catogories11.jpg";
+import vegetarian_img from "../img/catogories2.jpg";
+import vegan_img from "../img/catogories3.jpg";
 
 function Categories() {
   return (
     <div className="categories">
       <Link to="/Dishes/meat">
-        <div className="meat">Meat</div>
+        <div className="meat">
+          <img src={meat_img} id="meat_img" />
+          Meat
+        </div>
       </Link>
       <Link to="/Dishes/vegetarian">
-        <div className="vegetarian">Vegetarian</div>
+        <div className="vegetarian">
+          <img src={vegetarian_img} />
+          Vegetarian
+        </div>
       </Link>
       <Link to="/Dishes/vegan">
-        <div className="vegan">Vegen</div>
+        <div className="vegan">
+          <img src={vegan_img} />
+          Vegen
+        </div>
       </Link>
     </div>
   );
